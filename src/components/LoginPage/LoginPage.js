4c395be0a5a05843059dfe0a58b7d7ee4edd814b
Shawn Axsom <shawn.axsom@rooksecurity.com>
2015-08-30 14:51:02 -0400

    first commit

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,30 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { PropTypes } from 'react';
+import styles from './LoginPage.css';
+import withStyles from '../../decorators/withStyles';
+
+@withStyles(styles)
+class LoginPage {
+
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired
+  };
+
+  render() {
+    let title = 'Log In';
+    this.context.onSetTitle(title);
+    return (
+      <div className="LoginPage">
+        <div className="LoginPage-container">
+          <h1>{title}</h1>
+          <p>Hi</p>
+          <a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=229TPJ&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight">Login here</a>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default LoginPage;
